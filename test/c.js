9cf501590e48c75905274a9d81ec1ea17e54e593
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2016-06-05 19:27:10 +0200

    mvp .compile

--- test/c.js
+++ test/c.js
@@ -3,17 +3,21 @@ const assert = require('assert')
 const child_process = require('child_process')
 
 {
-  child_process.execSync('clang test/fixtures/sources/return_with_0.c -o test/fixtures/sources/return_with_0')
-  const e = child_process.spawn('./test/fixtures/sources/exit_with_1', []);
-  e.on('close', (code) => {
-    assert(code === 1, 'Compiled binary exit_with_1 must exit with code 1')
-  });
+  const out = 'test/fixtures/sources/c/out/exit_with_1'
+  pt.compile('test/fixtures/sources/c/exit_with_1.c', {output: out}, () => {
+    const e = child_process.spawn('./' + out, []);
+    e.on('close', (code) => {
+      assert(code === 1, 'Compiled binary exit_with_1 must exit with code 1')
+    });
+  })
 }
 
 {
-  child_process.execSync('clang test/fixtures/sources/exit_with_1.c -o test/fixtures/sources/exit_with_1')
-  const e = child_process.spawn('./test/fixtures/sources/return_with_0', []);
-  e.on('close', (code) => {
-    assert(code === 0, 'Compiled binary return_with_0 must exit with code 0')
-  });
+  const out = 'test/fixtures/sources/c/out/return_with_0'
+  pt.compile('test/fixtures/sources/c/return_with_0.c', {output: out}, () => {
+    const e = child_process.spawn('./' + out, []);
+    e.on('close', (code) => {
+      assert(code === 0, 'Compiled binary return_with_0 must exit with code 0')
+    });
+  })
 }
