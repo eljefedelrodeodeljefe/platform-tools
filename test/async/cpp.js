b31419c3006ab3f9bb910e46b1f17aef94f01a2e
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2016-06-05 20:54:05 +0200

    make simple cpp compile

---
+++ test/async/cpp.js
@@ -0,0 +1,25 @@
+'use strict'
+const pt = require('../../')
+const assert = require('assert')
+const child_process = require('child_process')
+
+{
+  let out = 'test/fixtures/sources/cpp/out/simple'
+  console.log('came here');
+  pt.compile('test/fixtures/sources/cpp/simple.cc', {output: out}, () => {
+    // const e = child_process.spawn('./' + out, []);
+    // e.on('close', (code) => {
+    //   assert(code === 1, 'Compiled binary simple.cc must exit with code 0')
+    // });
+  })
+}
+
+{
+  // let out = 'test/fixtures/sources/c/out/return_with_0'
+  // pt.compile('test/fixtures/sources/c/return_with_0.c', {output: out}, () => {
+  //   const e = child_process.spawn('./' + out, []);
+  //   e.on('close', (code) => {
+  //     assert(code === 0, 'Compiled binary return_with_0 must exit with code 0')
+  //   });
+  // })
+}
