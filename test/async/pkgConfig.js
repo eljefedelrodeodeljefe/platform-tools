5d90e0bb7ed5fdba0cc9628c842e94c0f86953c6
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2016-06-06 18:31:34 +0200

    adds a simple .pc parser
    the implementation is rather brute force.
    Please review ASAP

---
+++ test/async/pkgConfig.js
@@ -0,0 +1,50 @@
+'use strict'
+const pt = require('../../')
+const assert = require('assert')
+const child_process = require('child_process')
+
+{
+  pt.config('test/fixtures/pc/openssl.pc', (err, res) => {
+    if (err) {
+      assert(!err, 'must not call calback with error')
+    }
+    assert.deepEqual(res, {
+                              prefix: '/usr',
+                              exec_prefix: '/usr',
+                              libdir: '/usr/lib',
+                              includedir: '/usr/include',
+                              name: 'OpenSSL',
+                              description: 'Secure Sockets Layer and cryptography libraries and tools',
+                              version: '0.9.8zh',
+                              requires: null,
+                              libs: [ '-L/usr/lib', '-lssl', '-lcrypto', '-lz' ],
+                              cflags: [ '-I/usr/include' ]
+                            }, 'Parsing .pc yields an object in the correct format')
+  })
+}
+
+{
+  pt.config('openssl', (err, res) => {
+    if (err) {
+      assert(!err, 'must not call calback with error')
+    }
+    try {
+      delete res.version
+      assert.deepEqual(res, {
+                                prefix: '/usr',
+                                exec_prefix: '/usr',
+                                libdir: '/usr/lib',
+                                includedir: '/usr/include',
+                                name: 'OpenSSL',
+                                description: 'Sockets Layer and cryptography libraries and tools',
+                                requires: null,
+                                libs: [ '-L/usr/lib', '-lssl', '-lcrypto', '-lz' ],
+                                cflags: [ '-I/usr/include' ]
+                              }, 'Parsing .pc yields an object in the correct format')
+    } catch (e) {
+      console.warn('This is allowed to fail, since it tests local installatation of OpenSSL')
+      console.warn('It failed with:\n')
+      console.warn(e)
+    }
+  })
+}
